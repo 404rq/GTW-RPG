1e610ac0cfc63d77d0dee40b925bd8346c916393
albonius <info@albonius.com>
2014-10-17 18:48:05 +0200

    Current job templates
    Two of the jobs in GTWcivilians

---
+++ GTWfarmer/farmer.lua
@@ -0,0 +1,168 @@
+--[[ 
+********************************************************************************
+	Project:		GTW RPG [2.0.4]
+	Owner:			GTW Games 	
+	Location:		Sweden
+	Developers:		MrBrutus
+	Copyrights:		See: "license.txt"
+	
+	Website:		http://code.albonius.com
+	Version:		2.0.4
+	Status:			Stable release
+********************************************************************************
+]]--
+
+-- Object ids: 822 plant, 1454 bale
+-- Globals
+plants_age 		= { }
+player_plants 	= { }
+plants_pos		= {{{ }}}
+
+-- Finds the nearest plant
+function find_nearest(player)
+	local x,y,z = getElementPosition(player)
+	local dist = 16
+	if not plants_pos[player] then
+		plants_pos[player] = { }
+	end
+	for w=1, #plants_pos[player] do
+		if plants_pos[player][w][1] and plants_pos[player][w][2] and plants_pos[player][w][3] then
+			local lx,ly,lz = plants_pos[player][w][1],plants_pos[player][w][2],plants_pos[player][w][3]
+			if dist > getDistanceBetweenPoints3D( x,y,z, lx,ly,lz ) then
+				dist = getDistanceBetweenPoints3D( x,y,z, lx,ly,lz )
+			end
+		end
+	end
+	return dist or 16
+end
+
+-- Plant the seed
+function plantSeed( player )
+	local seed_moey = math.random(400,500)
+	if getPlayerTeam(player) == getTeamFromName("Civilians") and getElementData(player, "Occupation") == "Farmer" and 
+		getPlayerMoney(player) > seed_moey and getPedOccupiedVehicle(player) and getElementModel( getPedOccupiedVehicle( player )) == 531 
+		and player_plants[player] < 200 then	
+		-- Plant the seed
+		local x,y,z = getElementPosition(player)
+		if not plants_pos then
+			plants_pos = { }
+		end
+		if find_nearest(player) > 15 then
+			takePlayerMoney(player, seed_moey)
+			local plant = createObject( 822, x, y, z-1 )
+			local plantCol = createColSphere( x, y, z-2, 5 )
+			local blip = createBlip( x, y, z, 0, 1, 255, 0, 0, 150, 0, 100, player )
+			setObjectScale( plant, 0.005 )
+			if not plants_pos[player] then
+				plants_pos[player] = { }
+			end
+			if not plants_pos[player][player_plants[player]] then
+				plants_pos[player][player_plants[player]] = { }
+			end
+			
+			-- Make the plant grow up
+			plants_age[plant] = 0;
+			local currentPlantsCounter = player_plants[player]
+			plants_pos[player][currentPlantsCounter][1] = x
+			plants_pos[player][currentPlantsCounter][2] = y
+			plants_pos[player][currentPlantsCounter][3] = z
+			
+			local time_to_grow = math.random(285, 320)
+			setTimer( function() 
+				setObjectScale( plant, getObjectScale(plant)+0.005 )
+				plants_age[plant] = plants_age[plant] + 1
+			end, 4000, time_to_grow )
+			
+			-- Status message
+			exports.GTWtopbar:dm( "Farmer: Your seed has been planted, make sure it's safe from intruders!", player, 0, 255, 0 )
+			player_plants[player] = player_plants[player] + 1;
+			
+			-- Show status
+			setTimer( function() 
+				if isElement(blip) then
+					setBlipColor( blip, 0, 255, 0, 100 )
+				end
+			end, time_to_grow*4000, 1 )
+			
+			-- Clean up
+			setTimer( function() 
+				if isElement(plant) then
+					destroyElement(plant)
+					destroyElement(plantCol)
+					destroyElement(blip)
+					plants_age[plant] = nil
+					plants_pos[player][currentPlantsCounter][1] = nil
+					plants_pos[player][currentPlantsCounter][2] = nil
+					plants_pos[player][currentPlantsCounter][3] = nil
+					
+					-- Status message
+					if isElement(player) then
+						exports.GTWtopbar:dm( "Farmer: Your plant rotted and was destroyed", player, 255, 200, 0 )
+					end
+				end
+			end, 7200000, 1 )
+			
+			addEventHandler( "onColShapeHit", plantCol,
+    		function ( hitElement, matchingdimension )
+        		if hitElement and isElement(hitElement) and getElementType(hitElement) == "player" and 
+        			getPedOccupiedVehicle(hitElement) and getElementModel( getPedOccupiedVehicle( hitElement )) == 532 and
+					plants_age[plant] and plants_age[plant] > time_to_grow - 1 then
+					-- Clear and makes a bale
+        			plants_age[plant] = nil
+        			setElementModel(plant, 1454)
+        			setBlipColor( blip, 255, 200, 0, 100 )
+        			setElementCollisionsEnabled( plant, false )
+        			local px,py,pz = getElementPosition(plant)
+        			setElementPosition(plant, px,py,pz+1.1)
+        		
+        			-- Status message
+					exports.GTWtopbar:dm( "Farmer: One of your plants has been harvested", player, 255, 200, 0 )
+        		elseif hitElement and isElement(hitElement) and getElementType(hitElement) == "player" and 
+        			not getPedOccupiedVehicle(hitElement) and getElementModel(plant) == 1454 then
+        			-- Pay for the bale
+        			givePlayerMoney(hitElement,math.random(900,1100))
+        			
+        			-- Increase stats by 1/plant
+					local playeraccount = getPlayerAccount( hitElement )
+					local farmer_plants = getAccountData( playeraccount, "acorp_stats_plants_harvested" ) or 0
+					setAccountData( playeraccount, "acorp_stats_plants_harvested", farmer_plants + 1 )
+        			
+        			-- Status message
+					exports.GTWtopbar:dm( "Farmer: One of your plants has been sold", player, 255, 200, 0 )
+					player_plants[player] = player_plants[player] - 1
+        			if isElement(plant) then
+						destroyElement(plant)
+						destroyElement(plantCol)
+						destroyElement(blip)
+						plants_pos[player][currentPlantsCounter][1] = nil
+						plants_pos[player][currentPlantsCounter][2] = nil
+						plants_pos[player][currentPlantsCounter][3] = nil
+						plants_age[plant] = nil
+					end
+        		end
+    		end)
+    	else
+    		exports.GTWtopbar:dm( "Farmer: You can't plant your seed too close to eachothers", player, 255, 0, 0 )
+    	end
+    elseif player_plants[player] and player_plants[player] > 199 then
+    	exports.GTWtopbar:dm( "Farmer: You can't plant more seed right now, let your current plants be ready first", player, 255, 0, 0 )
+	end
+end
+addCommandHandler( "plant", plantSeed )
+
+for w,pl in pairs(getElementsByType("player")) do 
+	bindKey( pl, "n", "down", "plant" )
+	player_plants[pl] = 0
+end
+addEventHandler("onPlayerLogin", root,
+function()
+    bindKey( source, "n", "down", "plant" )
+    player_plants[source] = 0
+end)
+
+function enterVehicle( thePlayer, seat, jacked ) 
+    if getElementModel(source) == 531 and getElementData(thePlayer, "Occupation") == "Farmer" then 
+		exports.GTWtopbar:dm( "Farmer: Press n to plant your seed", thePlayer, 255, 200, 0 )
+    end
+end
+addEventHandler ( "onVehicleEnter", getRootElement(), enterVehicle )
---
+++ GTWfisher/fisher.lua
@@ -0,0 +1,178 @@
+--[[ 
+********************************************************************************
+	Project:		GTW RPG [2.0.4]
+	Owner:			GTW Games 	
+	Location:		Sweden
+	Developers:		MrBrutus
+	Copyrights:		See: "license.txt"
+	
+	Website:		http://code.albonius.com
+	Version:		2.0.4
+	Status:			Stable release
+********************************************************************************
+]]--
+
+-- Global data
+fisher_timer = nil
+fish_market_time = math.random(10000,30000)
+
+-- Fish data
+fish_last_pos = {{ }}
+fish_data = {
+	-- name, money
+	[1]={ "an Aligatorfish", 16 },
+	[2]={ "a Asian carp", 7 },
+	[3]={ "a Bandfish", 11 },
+	[4]={ "a Blue shark", 23 },
+	[5]={ "a Salmon", 45 },
+	[6]={ "a Norwegian Salmon", 67 },
+	[7]={ "a Pike", 23 },
+	[8]={ "a Perch", 24 },
+	[9]={ "a Black fish", 8 },
+	[10]={ "a Goldfish", 21 },
+	[11]={ "a Cat fish", 12 },
+	[12]={ "a Cow shark", 23 },
+	[13]={ "a Crestfish", 26 },
+	[14]={ "a Plaize", 14 },
+	[15]={ "a Coolie loach", 32 },
+	[16]={ "a Swedish Surströmming", 25 },
+	[17]={ "a Cobbler", 34 },
+	[18]={ "a Coffinfish", 17 },
+	[19]={ "a Dwarf gourami", 30 },
+	[20]={ "a Elver", 7 },
+	[21]={ "a Elephantnose fish", 23 },
+	[22]={ "a bag of weed", 156 },
+	[23]={ "a Eucla cod", 19 },
+	[24]={ "a Electric Eel", 23 },
+	[25]={ "a Emperior", 14 },
+	[26]={ "a Escolar", 14 },
+	[27]={ "a Elephant fish", 57 },
+	[28]={ "an anchor", 3 },
+	[29]={ "a Fire Goby", 29 },
+	[30]={ "a Four-eyed fish", 43 },
+	[31]={ "a Football fish", 31 },
+	[32]={ "a Frog fish", 4 },
+	[33]={ "a Frog", 75 },
+	[34]={ "a fish that has already been eaten", 1 },
+	[35]={ "a Frilled shark", 24 },
+	[36]={ "a French angel fish", 28 },
+	[37]={ "a Ground shark", 39 },
+	[38]={ "a Guppy", 4 },
+	[39]={ "a Gunnel", 21 },
+	[40]={ "a Goose fish", 33 },
+	[41]={ "a Golden throuth", 33 },
+	[42]={ "a bag of money", 42 },
+	[43]={ "a Ice fish", 21 },
+	[44]={ "a Horse fish", 22 },
+	[45]={ "a Hoki", 65 },
+	[46]={ "a Harelip sucker", 30 },
+	[47]={ "a Hairtail", 20 },
+	[48]={ "a Inconnu", 13 },
+	[49]={ "a Jewelfish", 91 },
+	[50]={ "a Knife fish", 34 },
+	[51]={ "a Ide", 17 },
+	[52]={ "a Killfish", 55 },
+	[53]={ "a Koi", 15 },
+	[54]={ "a Kokanee", 37 },
+	[55]={ "a piece of the lost city Atlantis", 99 },
+	[56]={ "a Japanese eel", 32 },
+	[57]={ "a IKEA chair", 42 },
+	[58]={ "a Lake chub", 27 },
+	[59]={ "a Luderick", 33 },
+	[60]={ "a Louvar", 21 },
+	[61]={ "a Longfin", 22 },
+	[62]={ "a Labyrinth fish", 11 },
+	[63]={ "a clam", 13 },
+	[64]={ "a broken clam", 2 },
+	[65]={ "a Long-finned pike", 28 },
+	[66]={ "something you didn't liked and threw back", 0 },
+	[67]={ "Lyretail", 33 },
+	[68]={ "a crab", 16 },
+	[69]={ "a Murray cod", 6 },
+	[70]={ "a Mud minnow", 8 },
+	[71]={ "a Mrigal", 3 },
+	[72]={ "a rock with the text \"Billy was here\"", 14 },
+	[73]={ "a Mud cat fish", 7 },
+	[74]={ "a Mullet", 5 },
+	[75]={ "the book \"nicke curious and the shark\"", 7 },
+	[76]={ "a shark", 1 },
+	[77]={ "a Milk fish", 9 },
+	[78]={ "the bottle of vodka you lost the last time you where here", 0 },
+	[79]={ "Mahseer", 12 },
+	[80]={ "a Monkfish", 25 },
+	[81]={ "a human hand", 3 },
+	[82]={ "a Nurse shark", 32 },
+	[83]={ "an eel", 3 },
+	[84]={ "a piraya", 7 },	
+	[85]={ "a North american darter", 21 },
+	[86]={ "a Nibble fish", 22 },
+	[87]={ "a Noodlefish", 18 },
+	[88]={ "a North american freshwater catfish", 36 },
+	[89]={ "a Oregon chub", 28 },
+	[90]={ "a Opah", 27 },
+	[91]={ "a Pink salmon", 123 },
+	[92]={ "a Porgy", 14 },
+	[93]={ "a Ponyfish", 18 },
+	[94]={ "a Oscar fish", 11 },
+	[95]={ "a Powen", 10 },
+	[96]={ "a Pacific cod", 20 },
+	[97]={ "a Pilot fish", 21 },
+	[98]={ "a Pygmy sunfish", 17 },
+	[99]={ "a Prickly shark", 32 },
+	[100]={ "a River shark", 23 },
+}
+
+-- Handle fishing
+function fishing_get()
+	local fishers = getElementsByType( "player" )	
+	for k,fisher in pairs(fishers) do 
+		if getPedContactElement( fisher ) and isElement( getPedContactElement( fisher )) and 
+			getElementType( getPedContactElement( fisher )) == "vehicle" and 
+			getVehicleType( getPedContactElement( fisher )) == "Boat" and
+			getPlayerTeam( fisher ) == getTeamFromName( "Civilians" ) and
+			getElementData( fisher, "Occupation" ) == "Fisher" then
+			-- Verify position
+			if not fish_last_pos[fisher] then
+				fish_last_pos[fisher] = { }
+				fish_last_pos[fisher][1],fish_last_pos[fisher][2],fish_last_pos[fisher][3] = 0,0,0
+			end
+			local x,y,z = getElementPosition( fisher )
+			local dist = getDistanceBetweenPoints3D( x,y,z, fish_last_pos[fisher][1],fish_last_pos[fisher][2],fish_last_pos[fisher][3] )
+			fish_last_pos[fisher][1],fish_last_pos[fisher][2],fish_last_pos[fisher][3] = x,y,z
+			
+			-- Check distance before payment
+			if dist > 0.1 then
+				-- This player is a fisher
+				local ID = math.random(#fish_data)
+				exports.GTWtopbar:dm( "You found "..fish_data[ID][1]..", worth "..fish_data[ID][2].."$", fisher, 0, 255, 0 )
+				givePlayerMoney( fisher, fish_data[ID][2] )
+			end
+		end
+	end	
+end
+
+-- Generates a new time
+function new_timer()
+	if isTimer( fisher_timer ) then
+		killTimer( fisher_timer )
+	end
+	fish_market = math.random(10000,30000)
+	fisher_timer = setTimer( fishing_get, fish_market, 0 )
+end
+
+function fish_market( player )
+	if fish_market_time < 14000 then
+		outputChatBox( "The fish population is excellent now, hurry up, go fishing!", player, 200, 200, 200 )
+	elseif fish_market_time < 18000 then
+		outputChatBox( "The fish are biting good now", player, 200, 200, 200 )
+	elseif fish_market_time < 24000 then
+		outputChatBox( "Fish market could have been worse but you might have some luck!", player, 200, 200, 200 )
+	else
+		outputChatBox( "You need a lot of fishing luck if you think you can get anything right now", player, 200, 200, 200 )
+	end
+end
+addCommandHandler( "fishmarket", fish_market )
+
+-- Set global timers
+fisher_timer = setTimer( fishing_get, fish_market_time, 0 )
+setTimer( new_timer, 3600000, 0 )
