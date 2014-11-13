fa8b5e2587c396c7ec9e3a552ff5f6eb9efa0944
albonius <info@albonius.com>
2014-11-13 18:32:18 +0100

    Walrus mechanic job
    Vehicle related functions for staff and the ability to work as mechanic
    by repair and refuel vehicles.

---
+++ GTWmechanic/mechanic-c.lua
@@ -0,0 +1,185 @@
+--[[ 
+********************************************************************************
+	Project:		GTW RPG [2.0.4]
+	Owner:			GTW Games 	
+	Location:		Sweden
+	Developers:		MrBrutus
+	Copyrights:		See: "license.txt"
+	
+	Website:		http://code.albonius.com
+	Version:		2.0.4
+	Status:			Stable release
+********************************************************************************
+]]--
+
+local element = nil
+local sx,sy = guiGetScreenSize()
+
+--[[ Make GUI for context menu ]]--
+addEventHandler("onClientResourceStart", getResourceRootElement(getThisResource()),
+function()	
+	window = exports.GTWgui:createWindow((sx-200)/2,(sy-300)/2, 200, 300, "Vehicle", false)
+	btn_repair = guiCreateButton(10, 30, 180, 40, "Repair", false, window)
+	btn_refuel = guiCreateButton(10, 70, 180, 40, "Refuel", false, window)
+	btn_close = guiCreateButton(10, 240, 180, 40, "Close", false, window)
+	-- Staff only
+	btn_enter = guiCreateButton(10, 110, 180, 40, "Enter vehicle", false, window)
+	btn_staff_repair = guiCreateButton(10, 150, 180, 40, "Fix and refuel", false, window)
+
+	-- Apply walrus font
+	exports.GTWgui:setDefaultFont(btn_repair, 10)
+	exports.GTWgui:setDefaultFont(btn_refuel, 10)
+	exports.GTWgui:setDefaultFont(btn_close, 10)
+	exports.GTWgui:setDefaultFont(btn_enter, 10)
+	exports.GTWgui:setDefaultFont(btn_staff_repair, 10)
+	guiSetVisible(window, false)
+	
+	-- Button click events
+	addEventHandler("onClientGUIClick", btn_repair, repair_destroy)
+	addEventHandler("onClientGUIClick", btn_refuel, refuel_information)
+	addEventHandler("onClientGUIClick", btn_close, close_win)
+	addEventHandler("onClientGUIClick", btn_enter, enter_veh)
+	addEventHandler("onClientGUIClick", btn_staff_repair, staff_repair)
+end)
+
+--[[ Update GUI depending on caller ]]--
+function openVehicleMenu(button, state, absoluteX, absoluteY, worldX, worldY, worldZ, clickedElement)
+    if not clickedElement then return end
+    local dist = getDistanceBetweenPoints3D(worldX, worldY, worldZ, getElementPosition(localPlayer)) or 0
+    if getElementType(clickedElement) == "vehicle" and(not getPedOccupiedVehicle(localPlayer) or
+        getPedOccupiedVehicle(localPlayer) ~= clickedElement) and 
+       ((getPlayerTeam(localPlayer) == getTeamFromName("Civilians") and 
+        getElementData(localPlayer, "Occupation") == "Mechanic") or
+       (getElementData(localPlayer, "Occupation") == "Admin" or 
+		getElementData(localPlayer, "Occupation") == "Developer" or
+		getElementData(localPlayer, "Occupation") == "Moderator")) and 
+		(dist < 20) and not guiGetInputEnabled() then
+        -- Open GUI as staff
+        if getElementData(localPlayer, "Occupation") ~= "Mechanic" then
+        	guiSetText(btn_repair, "Destroy")
+        	guiSetText(btn_refuel, "Information")
+        	guiSetVisible(btn_enter, true)
+        	guiSetVisible(btn_staff_repair, true)
+        -- Open GUI as mechanic
+        else
+        	guiSetText(btn_repair, "Repair")
+        	guiSetText(btn_refuel, "Refuel")
+        	guiSetVisible(btn_enter, false)
+        	guiSetVisible(btn_staff_repair, false)
+        end
+        element = clickedElement
+        showCursor(true)
+        guiSetVisible(window, true)
+    end
+end
+addEventHandler("onClientClick", root, openVehicleMenu)
+
+--[[ Repair or destroy?, that's the question ]]--
+function repair_destroy()
+	if not isElement(element) or not guiGetEnabled(btn_repair) or not source == btn_repair then return end
+	if getElementData(localPlayer, "Occupation") == "Mechanic" then			
+		if getElementData(element, "owner") then
+			local repairTime = (1000-getElementHealth(element))*30
+			if repairTime < 5000 then
+				repairTime = 5000
+			end
+			mech_pb = guiCreateProgressBar(0.4, 0.9, 0.2, 0.05, true)
+			setTimer(function()
+				if isElement(mech_pb) then
+					local progress = guiProgressBarGetProgress(mech_pb)
+					if progress < 99 then
+						guiProgressBarSetProgress(mech_pb, progress+1)
+					end
+				end
+			end, math.floor(repairTime/100), 100)  -- Progress maximum is 100
+			setTimer(destroyElement, repairTime, 1, mech_pb)
+			triggerServerEvent("GTWmechanic.repair", root, element, repairTime)
+		else
+			topBarMessage("This vehicle has no owner!", 255, 0, 0)
+		end
+	else
+		triggerServerEvent("GTWmechanic.destroy", getRootElement(), element)
+	end
+	guiSetVisible(window, false)
+	showCursor(false)
+end
+
+--[[ Refule or display information? that's another question ]]--
+function refuel_information()
+	if not isElement(element) or not guiGetEnabled(btn_refuel) or not source == btn_refuel then return end
+	if getElementData(localPlayer, "Occupation") == "Mechanic" then			
+		if getElementData(element, "owner") then
+			local fuel = getElementData(element, "vehicleFuel") or 0
+			local refuleTime = (100-fuel)*100
+			if refuleTime < 5000 then
+				refuleTime = 5000
+			end
+			mech_pb = guiCreateProgressBar(0.4, 0.9, 0.2, 0.05, true)
+			setTimer(function()
+				if isElement(mech_pb) then
+					local progress = guiProgressBarGetProgress(mech_pb)
+					if progress < 99 then
+						guiProgressBarSetProgress(mech_pb, progress+1)
+					end
+				end
+			end, math.floor(refuleTime/100), 100)  -- Progress maximum is 100
+			setTimer(destroyElement, refuleTime, 1, mech_pb)
+			triggerServerEvent("GTWmechanic.refuel", root, element, refuleTime)
+		else
+			topBarMessage("This vehicle has no owner!", 255, 0, 0)
+		end
+	else
+		--exports.GTWtopbar:dm("Vehicle was sucsessfully removed!", 0, 255, 0)
+		local owner = getElementData(element, "owner")
+		local fuel = getElementData(element, "vehicleFuel")
+		local health = getElementHealth(element)
+		if owner and fuel and health then
+			outputChatBox("INFO: owner="..owner..", fuel="..fuel..", health="..health, 255, 255, 255)
+		else
+			outputChatBox("INFO: no information available", 255, 255, 255)
+		end
+	end
+	guiSetVisible(window, false)
+	showCursor(false)
+end
+
+--[[ Close the window, just close it ]]--
+function close_win()
+	showCursor(false)
+	guiSetVisible(window, false)
+end
+
+--[[ Ability for staff to enter any vehicle ]]--
+function enter_veh()
+	if guiGetEnabled(btn_enter) and source == btn_enter then
+		guiSetVisible(window, false)
+		showCursor(false)
+		if isElement(element) and getElementData(localPlayer, "staff") then
+			triggerServerEvent("GTWmechanic.staff.enter", root, element)
+		end
+	end
+end
+
+--[[ Ability for staff to fix and repair ]]--
+function staff_repair()
+	if guiGetEnabled(btn_staff_repair) and source == btn_staff_repair then
+		guiSetVisible(window, false)
+		showCursor(false)
+		if isElement(element) and getElementData(localPlayer, "staff") then
+			triggerServerEvent("GTWmechanic.staff.repair", root, element)
+		end
+	end
+end
+
+--[[ Extra feature to make everything more comfortable ]]--
+function topBarMessage(message, r, g, b)
+    exports.GTWtopbar:dm(message, r, g, b)
+end
+function toggleCursor()
+	if isCursorShowing(localPlayer) then
+    	showCursor(false, false)
+    else
+    	showCursor(true, true)
+    end
+end
+bindKey("x", "down", toggleCursor, "Toggle cursor")
---
+++ GTWmechanic/mechanic-s.lua
@@ -0,0 +1,154 @@
+--[[ 
+********************************************************************************
+	Project:		GTW RPG [2.0.4]
+	Owner:			GTW Games 	
+	Location:		Sweden
+	Developers:		MrBrutus
+	Copyrights:		See: "license.txt"
+	
+	Website:		http://code.albonius.com
+	Version:		2.0.4
+	Status:			Stable release
+********************************************************************************
+]]--
+
+local default_money = 428  	-- Global definition based on what mechanics earn IRL/year 
+							-- divided by 12 and then 31 as in days multiplied by 4
+
+--[[ Pay for repair and refuel ]]--
+function pay_repair(mech, owner)
+	local pacc = getPlayerAccount(mech)
+	local repaired_cars = getAccountData(pacc, "acorp_stats_repaired_cars") or 0
+	if isElement(owner) then
+		takePlayerMoney(owner, default_money*3)
+	end
+	givePlayerMoney(mech, (default_money*2)+repaired_cars)
+end
+function pay_refuel(mech, owner)
+	local pacc = getPlayerAccount(mech)
+	local repaired_cars = getAccountData(pacc, "acorp_stats_repaired_cars") or 0
+	if owner and isElement(owner) then
+		takePlayerMoney(owner, default_money)
+	end
+	givePlayerMoney(mech, (default_money/2)+repaired_cars)
+end
+
+--[[ Repair vehicle as mechanic ]]--
+function repair_veh(veh, repairTime)
+	if not veh or not isElement(veh) then outPutTopbarMessage("No vehicle found", client, 255, 0, 0) return end
+	local acc = getElementData(veh, "owner")
+	if not acc then return end
+	local owner = getAccountPlayer(getAccount(acc))
+	if not owner then outPutTopbarMessage("The owner of this vehicle is currently offline!", client, 255, 0, 0) return end
+	
+	-- Freeze elements during repair
+	setElementFrozen(veh, true)
+	setElementFrozen(client, true)
+	setPedAnimation(client, "GRAFFITI", "spraycan_fire", -1, true, false)
+	showCursor(client, true)
+	outPutTopbarMessage("Reparing vehicle...", client, 0, 255, 0)
+	outPutTopbarMessage("Your vehicle is repaired...", owner, 0, 255, 0)
+	
+	-- Reset after repair
+	setTimer(fixVehicle, math.floor(repairTime), 1, veh)
+	setTimer(showCursor, math.floor(repairTime), 1, client, false)
+	setTimer(setElementFrozen, math.floor(repairTime), 1, veh, false)
+	setTimer(setElementFrozen, math.floor(repairTime), 1, client, false)
+	setTimer(outPutTopbarMessage, math.floor(repairTime), 1, "Vehicle was sucsessfully repaired!", client, 0, 255, 0)
+	setTimer(outPutTopbarMessage, math.floor(repairTime), 1, "Your vehicle was repaired by: "..getPlayerName(client), owner, 0, 255, 0)
+	setTimer(pay_repair, math.floor(repairTime), 1, client, owner)
+	setTimer(setPedAnimation, math.floor(repairTime), 1, client, nil, nil)
+				
+	-- Increase stats by 1
+	local playeraccount = getPlayerAccount(client)
+	local repaired_cars = getAccountData(playeraccount, "acorp_stats_repaired_cars") or 0
+	setAccountData(playeraccount, "acorp_stats_repaired_cars", repaired_cars + 1)
+end
+addEvent("GTWmechanic.repair", true)
+addEventHandler("GTWmechanic.repair", root, repair_veh)
+
+--[[ Refule as mechanic ]]--
+function refuel_veh(veh, refuelTime)
+	if not veh or not isElement(veh) then outPutTopbarMessage("No vehicle found", client, 255, 0, 0) return end
+	local acc = getElementData(veh, "owner")
+	if not acc then return end
+	local owner = getAccountPlayer(getAccount(acc))
+	if not owner then outPutTopbarMessage("The owner of this vehicle is currently offline!", client, 255, 0, 0) return end
+	
+	-- Freeze elements during repair
+	setElementFrozen(veh, true)
+	setElementFrozen(client, true)
+	setPedAnimation(client, "GRAFFITI", "spraycan_fire", -1, true, false)
+	showCursor(client, true)
+	outPutTopbarMessage("Refueling vehicle...", client, 0, 255, 0)
+	outPutTopbarMessage("Your vehicle is being refuled...", owner, 0, 255, 0)
+	
+	-- Reset after repair
+	setTimer(showCursor, math.floor(refuelTime), 1, client, false)
+	setTimer(setElementFrozen, math.floor(refuelTime), 1, veh, false)
+	setTimer(setElementFrozen, math.floor(refuelTime), 1, client, false)
+	setTimer(outPutTopbarMessage, math.floor(refuelTime), 1, "Vehicle was sucsessfully refuled!", client, 0, 255, 0)
+	setTimer(outPutTopbarMessage, math.floor(refuelTime), 1, "Your vehicle was refuled by: "..getPlayerName(client), owner, 0, 255, 0)
+	setTimer(setPedAnimation, math.floor(refuelTime), 1, client, nil, nil)
+	setTimer(pay_refuel, math.floor(refuelTime), 1, client, owner)
+	setTimer(setElementData, math.floor(refuelTime), 1, veh, "vehicleFuel", 100)
+end
+addEvent("GTWmechanic.refuel", true)
+addEventHandler("GTWmechanic.refuel", root, refuel_veh)
+
+--[[ Fix and repair as staff ]]--
+function staff_repair(veh)
+	local accName = getAccountName(getPlayerAccount(client))
+    if not (isObjectInACLGroup("user."..accName, aclGetGroup("Admin")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Developer")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Moderator"))) then
+		outPutTopbarMessage("You are not allowed to use this feature!", client, 255, 0, 0)    	
+    	return 
+    end
+	if not veh or not isElement(veh) then return end
+	outPutTopbarMessage("Vehicle was sucsessfully repaired!", client, 0, 255, 0)
+	setElementData(veh, "vehicleFuel", 100)
+	fixVehicle(veh)
+end
+addEvent("GTWmechanic.staff.repair", true)
+addEventHandler("GTWmechanic.staff.repair", root, staff_repair)
+
+--[[ Enter any vehicle as staff ]]--
+function staff_enter(veh)
+	local accName = getAccountName(getPlayerAccount(client))
+    if not (isObjectInACLGroup("user."..accName, aclGetGroup("Admin")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Developer")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Moderator"))) then
+		outPutTopbarMessage("You are not allowed to use this feature!", client, 255, 0, 0)    	
+    	return 
+    end
+	if not veh or not isElement(veh) then return end
+	warpPedIntoVehicle(client, veh)
+end
+addEvent("GTWmechanic.staff.enter", true)
+addEventHandler("GTWmechanic.staff.enter", root, staff_enter)
+
+--[[ Staff destroy vehicle ]]--
+function staff_destroy(veh)
+	local accName = getAccountName(getPlayerAccount(client)) 
+	if not (isObjectInACLGroup("user."..accName, aclGetGroup("Admin")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Developer")) or 
+    	isObjectInACLGroup("user."..accName, aclGetGroup("Moderator"))) then
+		outPutTopbarMessage("You are not allowed to use this feature!", client, 255, 0, 0)    	
+    	return 
+    end
+	if not veh or not isElement(veh) then return end
+	if getElementData(veh,"owner") then
+		outPutTopbarMessage("Vehicle was sucsessfully removed!", client, 0, 255, 0)
+		outputServerLog("VEH_ADMIN: Vehicle was removed at: "..getZoneName(getElementPosition(veh))..
+			", by: "..getPlayerName(client).." owner was: "..getElementData(veh,"owner"))
+	end
+	
+	-- Clean up if owned vehicle bought in shop
+	triggerEvent("GTWvehicleshop.onPlayerVehicleDestroy", root, veh, true)
+end
+addEvent("GTWmechanic.destroy", true)
+addEventHandler("GTWmechanic.destroy", root, staff_destroy)
+function outPutTopbarMessage(message, thePlayer, r, g, b)
+	exports.GTWtopbar:dm(message, thePlayer, r, g, b)
+end
