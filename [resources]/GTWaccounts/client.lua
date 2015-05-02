8b8e8d3ae680c941602105d87e9ca883eb67874a
The Moose <themoose@games.albonius.com>
2015-05-02 19:12:39 +0200

    Added GTWaccounts
    GTWaccounts manage spawns, login panel, invite bonuses and more.

---
+++ [resources]/GTWaccounts/client.lua
@@ -0,0 +1,167 @@
+--[[ 
+********************************************************************************
+	Project owner:		GTWGames												
+	Project name: 		GTW-RPG	
+	Developers:   		GTWCode
+	
+	Source code:		https://github.com/GTWCode/GTW-RPG/
+	Bugtracker: 		http://forum.gtw-games.org/bug-reports/
+	Suggestions:		http://forum.gtw-games.org/mta-servers-development/
+	
+	Version:    		Open source
+	License:    		GPL v.3 or later
+	Status:     		Stable release
+********************************************************************************
+]]--
+
+-- Global client properites
+p_account = nil
+p_loggedIn = false
+
+-- Display status messages from the server
+addEvent("GTWaccounts:onStatusReceive", true)
+addEventHandler("GTWaccounts:onStatusReceive", root, function(msg, statusCode)
+	if statusCode == -1 then
+		guiLabelSetColor(labelInfo, 255, 0, 0)
+	elseif statusCode == 0 then
+		guiLabelSetColor(labelInfo, 255, 255, 0)
+	elseif statusCode == 1 then
+		guiLabelSetColor(labelInfo, 0, 255, 0)
+	end
+	guiSetText(labelInfo, msg)
+end)
+
+-- Setup the login GUI (GTWgui must be running)
+function CreateLoginScreen()
+	x,y = guiGetScreenSize()
+	window = exports.GTWgui:createWindow((x-350)/2, (y-450)/2, 350, 450, "Grand Theft Walrus", false )
+	loginButton = guiCreateButton(230,400,100,40,"Login",false,window)
+	registerButton = guiCreateButton(128,400,100,40,"Signup",false,window)
+	updatesButton = guiCreateButton(10,400,116,40,"What's new",false,window)
+	labelUser = guiCreateLabel(30, 40, 290, 25, "Username:", false, window)
+	labelPwrd = guiCreateLabel(30, 100, 290, 25, "Password:", false, window)
+	textUser = guiCreateEdit(30,65,290,25,"",false,window)
+	textPwrd = guiCreateEdit(30,125,290,25,"",false,window)
+	labelInfo = guiCreateLabel(30, 210, 290, 80, "", false, window)
+	checkBoxUser = guiCreateCheckBox(30, 160, 290, 20, "Remember Username", false, false, window)
+	checkBoxPwrd = guiCreateCheckBox(30, 180, 290, 20, "Remember Password", false, false, window)
+	guiEditSetMasked(textPwrd, true)
+	guiSetInputEnabled(true)
+	
+	-- Friends invite bonus
+	labelFacc = guiCreateLabel(30, 300, 190, 25, "Send your friend a gift", false, window)
+	textFacc = guiCreateEdit(30,325,260,25,"",false,window)
+	faccButtonHelp = guiCreateButton(290,325,30,25,"?",false,window)
+	
+	-- Set GUI font
+	exports.GTWgui:setDefaultFont(loginButton, 10)
+	exports.GTWgui:setDefaultFont(registerButton, 10)
+	exports.GTWgui:setDefaultFont(updatesButton, 10)
+	exports.GTWgui:setDefaultFont(labelUser, 10)
+	exports.GTWgui:setDefaultFont(labelPwrd, 10)
+	exports.GTWgui:setDefaultFont(labelInfo, 10)
+	exports.GTWgui:setDefaultFont(textUser, 9)
+	exports.GTWgui:setDefaultFont(textPwrd, 9)
+	exports.GTWgui:setDefaultFont(checkBoxUser,10)
+	exports.GTWgui:setDefaultFont(checkBoxPwrd, 10)
+	exports.GTWgui:setDefaultFont(labelFacc, 10)
+	exports.GTWgui:setDefaultFont(textFacc, 10)
+	exports.GTWgui:setDefaultFont(faccButtonHelp, 10)
+	showCursor(true)
+	
+	-- Load login details from xml
+	local f = xmlLoadFile('@data.xml', 'account')
+	if f then
+		local user = xmlNodeGetAttribute(xmlFindChild(f, 'user', 0), 'value')
+		local pass = xmlNodeGetAttribute(xmlFindChild(f, 'pass', 0), 'value')
+		guiSetText(textUser, tostring(user))
+		guiSetText(textPwrd, tostring(pass))
+		if user ~= "" then
+			guiCheckBoxSetSelected(checkBoxUser, true)
+		end 
+		if pass ~= "" then
+			guiCheckBoxSetSelected(checkBoxPwrd, true)
+		end
+	end
+end
+
+-- Login GUI click events
+addEventHandler("onClientGUIClick",root,function() 
+	-- On login (asyncron function)
+	if source == loginButton then
+		guiLabelSetColor(labelInfo, 255, 255, 255)
+		guiSetText(labelInfo, "Attempting to login...")
+		fadeCamera(false, 1)
+		setTimer(triggerServerEvent, 1100, 1, "GTWaccounts:attemptClientLogin", localPlayer, guiGetText(textUser), guiGetText(textPwrd))
+	-- On registration
+	elseif source == registerButton then
+		triggerServerEvent("GTWaccounts:onClientAttemptRegistration", localPlayer, guiGetText(textUser), guiGetText(textPwrd), guiGetText(textFacc))
+	-- On view updates button click (requires GTWupdates)
+	elseif source == updatesButton then
+		exports.GTWupdates:viewUpdateListGUI()
+	-- On help request click
+	elseif source == faccButtonHelp then
+		guiSetText(labelInfo, "Enter your friends account name \nto send him/her an invite bonus\nof $4'000 as a reward.")
+		guiLabelSetColor(labelInfo, 255, 255, 255)
+	end
+end)
+
+-- On client attempt login
+addEvent("GTWaccounts:onClientPlayerLogin", true)
+addEventHandler("GTWaccounts:onClientPlayerLogin", root, function(acnt)
+	exports.GTWtopbar:dm("Welcome to Grand Theft Walrus, "..getPlayerName(localPlayer).."!", 255, 255, 255)
+	local f = xmlCreateFile("@data.xml", "account")
+	local user, pass = "", "" 
+	if guiCheckBoxGetSelected(checkBoxUser) then
+		user = guiGetText(textUser)
+	end 
+	if guiCheckBoxGetSelected(checkBoxPwrd) then
+		pass = guiGetText(textPwrd)
+	end
+	xmlNodeSetAttribute(xmlCreateChild(f, "user"), "value", user)
+	xmlNodeSetAttribute(xmlCreateChild(f, "pass"), "value", pass)
+	xmlSaveFile(f)
+	xmlUnloadFile(f)
+	guiSetVisible(window, false)
+	showCursor(false)
+	guiSetInputEnabled(false)
+	showChat(true)
+	
+	-- Set account obtained from the server and login status
+	p_loggedIn = true
+	p_account = acnt
+end)
+
+-- Exported functions to check if logged in or to obtain account name
+function getPlayerAccount()
+	return p_account or "Guest"
+end
+function isClientLoggedIn()
+	return p_loggedIn
+end
+
+-- Get a players ID
+function getPlayerId(p)
+	local id = nil
+	for i, v in pairs(getElementsByType('player')) do
+		if v == p then
+			id = i
+			break
+		end
+	end
+	return id
+end
+
+-- Display login screen to players who isn't currently logged in
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	if not getElementData(localPlayer, "isLoggedIn") then
+		setTimer(CreateLoginScreen, 1000, 1)
+		showChat(false)
+	end
+end)
+
+-- Notify server about any restarted client resource
+addEventHandler("onClientResourceStart", root,
+function(startedRes)
+    triggerServerEvent("onClientSend",localPlayer)
+end)
---
+++ [resources]/GTWaccounts/server.lua
@@ -0,0 +1,146 @@
+--[[ 
+********************************************************************************
+	Project owner:		GTWGames												
+	Project name: 		GTW-RPG	
+	Developers:   		GTWCode
+	
+	Source code:		https://github.com/GTWCode/GTW-RPG/
+	Bugtracker: 		http://forum.gtw-games.org/bug-reports/
+	Suggestions:		http://forum.gtw-games.org/mta-servers-development/
+	
+	Version:    		Open source
+	License:    		GPL v.3 or later
+	Status:     		Stable release
+********************************************************************************
+]]--
+
+-- Attempts cache
+login_cache = { }
+
+-- On login request
+addEvent("GTWaccounts:attemptClientLogin", true)
+addEventHandler("GTWaccounts:attemptClientLogin", root, function(user, pass)
+	if isGuestAccount(getPlayerAccount(source)) then
+		if not getAccount(user) then
+			return doStatus("This account doesn't exist\non the server.", source, 0)
+		end		
+		if not logIn(source, accnt, pass) then
+			return doStatus("Incorrect password for\nthis account.", source, -1)
+		end
+		triggerClientEvent(source, "GTWaccounts:onClientPlayerLogin", source, user)
+	else
+		triggerClientEvent(source, "GTWaccounts:onClientPlayerLogin", source, getAccountName(getPlayerAccount(source)))
+	end
+end)
+
+-- On register request
+addEvent("GTWaccounts:onClientAttemptRegistration", true)
+addEventHandler("GTWaccounts:onClientAttemptRegistration", root, function(user, pass, facc)
+	if user and pass then
+		local char = "[^0-9a-zA-Z_]"
+		user = string.gsub(user, char, "")
+		local acn = getAccount(user)
+		if acn then
+			return doStatus("This account already exists\non the server.", client, -1)
+		end
+		local friend = getAccount(facc)
+		local acn = addAccount(user, pass)
+		if acn then
+			doStatus("You have registered! \nYou can now login.", source, 1)
+			if friend and acn ~= friend and not getAccountData(acn, "GTWaccounts.invite.acc") and
+				getAccountData(friend, "GTWaccounts.invite.serial") ~= getPlayerSerial(client) and
+				getAccountData(friend, "GTWaccounts.invite.ip") ~= getPlayerIP(client) then
+				local friendPlayer = getAccountPlayer(friend)
+				if friendPlayer then
+					givePlayerMoney(friendPlayer, 4000)
+					outputChatBox("#66FF00[GTW]#FFFFFF Your friend just joined the server, here's your welcome bonus of 4'000$", friendPlayer, 255, 255, 255, true)
+					setAccountData(acn, "GTWaccounts.invite.acc", facc)
+					-- Friend has received money from ip and serial ...
+					if friend then
+						setAccountData(friend, "GTWaccounts.invite.serial", getPlayerSerial(client))
+						setAccountData(friend, "GTWaccounts.invite.ip", getPlayerIP(client))
+					end
+				else
+					setAccountData(friend, "acorp.money", (getAccountData(friend, "acorp.money") or 0) + 4000)
+					setAccountData(acn, "GTWaccounts.invite.acc", facc)
+					-- Friend has received money from ip and serial ...
+					if friend then
+						setAccountData(friend, "GTWaccounts.invite.serial", getPlayerSerial(client))
+						setAccountData(friend, "GTWaccounts.invite.ip", getPlayerIP(client))
+					end
+				end
+			else
+				exports.GTWtopbar:dm("A welcome bonus has already been sent to this player", client, 255, 100, 0)
+			end
+		else
+			doStatus("Failed to add account.", client, -1)
+		end
+	else
+		doStatus("Expected username and password.", client, 0)
+	end
+end)
+
+-- Manage the invite bonus
+function sendInviteBonus(thePlayer, cmd, facc)
+	local friend = getAccount(facc)
+	if friend and facc then
+		local acn = getPlayerAccount(thePlayer)
+		if acn ~= friend and not getAccountData(acn, "GTWaccounts.invite.acc") and 
+			getAccountData(friend, "GTWaccounts.invite.serial") ~= getPlayerSerial(thePlayer) and
+			getAccountData(friend, "GTWaccounts.invite.ip") ~= getPlayerIP(thePlayer) then
+			local friendPlayer = getAccountPlayer(friend)
+			if friendPlayer then
+				givePlayerMoney(friendPlayer, 4000)
+				outputChatBox("#66FF00[GTW]#FFFFFF Your friend just joined the server, here's youre welcome bonus of 4'000$", friendPlayer, 255, 255, 255, true)
+				setAccountData(acn, "GTWaccounts.invite.acc", facc)
+				-- Friend has received money from ip and serial ...
+				setAccountData(friend, "GTWaccounts.invite.serial", getPlayerSerial(thePlayer))
+				setAccountData(friend, "GTWaccounts.invite.ip", getPlayerIP(thePlayer))
+			else
+				setAccountData(friend, "acorp.money", (getAccountData(friend, "acorp.money") or 0) + 4000)
+				setAccountData(acn, "GTWaccounts.invite.acc", facc)
+				-- Friend has received money from ip and serial ...
+				setAccountData(friend, "GTWaccounts.invite.serial", getPlayerSerial(thePlayer))
+				setAccountData(friend, "GTWaccounts.invite.ip", getPlayerIP(thePlayer))
+			end
+		else
+			exports.GTWtopbar:dm("Welcome bonus has already been sent to this player", thePlayer, 255, 100, 0)
+		end
+	else
+		outputChatBox("Correct syntax: /sendinvitebonus <friends_account_name>", thePlayer, 255, 255, 255)
+	end
+end
+addCommandHandler("sendinvitebonus", sendInviteBonus)
+
+-- Send status message to clients, code -1 = red, 0 = yellow, 1 = green
+function doStatus(msg, p, statusCode)
+	return triggerClientEvent(p, "GTWaccounts:onStatusReceive", p, msg, statusCode)
+end
+
+-- Set player unique ID on login
+addEventHandler("onPlayerJoin", resourceRoot, function()
+	setElementData(source, "ID", tostring(getPlayerId(source)))
+end)
+
+-- Give all players their unique ID once resource is restarted
+addEventHandler("onResourceStart", resourceRoot, function()
+	for i, v in ipairs(getElementsByType("player")) do
+		setElementData(v, "ID", tostring(i))
+	end
+end)
+
+-- Helper function to obtain a players IP
+function getPlayerId(p)
+	for i, v in pairs(getElementsByType("player")) do
+		if p == v then
+			return i
+		end
+	end
+end
+
+-- Fade camera and set the player as target on login
+addEventHandler("onPlayerLogin", root,
+function()
+    fadeCamera(source, true, 5)
+	setCameraTarget(source, source)
+end)
