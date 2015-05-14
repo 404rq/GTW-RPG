3fe40e56e3ca806a7effc11e57b48170011b62af
The Moose <themoose@games.albonius.com>
2015-05-14 17:15:27 +0200

    Added nametags
    * New feature, custom nametags that only display the name and hides it
    if a player is invisible.

---
+++ [resources]/GTWnametags/nametag_c.lua
@@ -0,0 +1,40 @@
+--[[ 
+********************************************************************************
+	Project owner:		GTWGames												
+	Project name:		GTW-RPG	
+	Developers:			GTWCode
+	
+	Source code:		https://github.com/GTWCode/GTW-RPG/
+	Bugtracker:			http://forum.gtw-games.org/bug-reports/
+	Suggestions:		http://forum.gtw-games.org/mta-servers-development/
+	
+	Version:			Open source
+	License:			GPL v.3 or later
+	Status:				Stable release
+********************************************************************************
+]]--
+
+local x,y = guiGetScreenSize()
+function update_nametags()
+	for k,v in pairs(getElementsByType("player")) do
+		if v ~= localPlayer and getPlayerTeam(v) then
+			local r,g,b = getTeamColor(getPlayerTeam(v))
+			local x1,y1,z1 = getElementPosition(localPlayer)
+			local x2,y2,z2 = getElementPosition(v)
+			local visibleto = getDistanceBetweenPoints3D(x1,y1,z1,x2,y2,z2)
+			if visibleto < 10 and getElementData(localPlayer, "isLoggedIn") then
+				local sx,sy = getScreenFromWorldPosition(x2,y2,z2+0.4)
+				if sx and sy and not getElementData(v, "anon") and getElementInterior(v) == getElementInterior(localPlayer) and 
+					getElementDimension(v) == getElementDimension(localPlayer) then
+					local nWidth = string.len(getPlayerName(v)) or 20
+					--dxDrawRectangle( sx-((nWidth*14)/2),sy-7,(nWidth*14),30, tocolor(0,0,0,100))
+					dxDrawText( string.gsub( getPlayerName(v), "#%x%x%x%x%x%x", "" ).."", sx,sy,sx,sy, 
+						tocolor(0,0,0,200), 0.6, "bankgothic", "center","top",false,false,false )
+					dxDrawText( string.gsub( getPlayerName(v), "#%x%x%x%x%x%x", "" ).."", sx+2,sy+2,sx,sy, 
+						tocolor(r,g,b,255), 0.6, "bankgothic", "center","top",false,false,false )
+				end
+			end
+		end
+	end
+end
+addEventHandler("onClientRender", root, update_nametags)
