bb8cc60146af541ddee0545b8530390ad70f2ace
The Moose <themoose@games.albonius.com>
2015-05-17 07:42:42 +0200

    [New] Safe areas will control and fire rockets
    * Protection from flying objects approaching at jail and area 51

---
+++ [resources]/GTWsafeareas/safearea_s.lua
@@ -0,0 +1,20 @@
+--[[ 
+********************************************************************************
+	Project owner:		GTWGames												
+	Project name:		GTW-RPG	
+	Developers:			GTWCode
+	
+	Source code:		https://github.com/GTWCode/GTW-RPG/
+	Bugtracker:			http://forum.albonius.com/bug-reports/
+	Suggestions:		http://forum.albonius.com/mta-servers-development/
+	
+	Version:			Open source
+	License:			GPL v.3 or later
+	Status:				Stable release
+********************************************************************************
+]]--
+
+-- Redcue damage made by minigun
+setWeaponProperty(38, "pro", "damage", 2)
+setWeaponProperty(38, "std", "damage", 2)
+setWeaponProperty(38, "poor", "damage", 2)
