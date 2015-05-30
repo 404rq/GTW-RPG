d38ec96afef517f2aec6518cbcc80c6e6115501a
The Walrus <TheWalrus-GTW@users.noreply.github.com>
2015-05-30 20:42:33 +0200

    Update head_officer.lua

--- [resources]/GTWpolicechief/head_officer.lua
+++ [resources]/GTWpolicechief/head_officer.lua
@@ -2,15 +2,15 @@
 ********************************************************************************
 	Project owner:		GTWGames												
 	Project name:		GTW-RPG	
-	Developers:			GTWCode
+	Developers:		GTWCode
 	
 	Source code:		https://github.com/GTWCode/GTW-RPG/
-	Bugtracker:			http://forum.albonius.com/bug-reports/
+	Bugtracker:		http://forum.albonius.com/bug-reports/
 	Suggestions:		http://forum.albonius.com/mta-servers-development/
 	
-	Version:			Open source
-	License:			GPL v.3 or later
-	Status:				Stable release
+	Version:		Open source
+	License:		GPL v.3 or later
+	Status:			Stable release
 ********************************************************************************
 ]]--
 
