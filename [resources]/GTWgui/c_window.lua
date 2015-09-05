fd92454ee61a846767f00bcc5ffb111d03fc1e5b
Mr_Moose <themoose@404rq.com>
2015-09-05 14:40:56 +0200

    [Patch] Moved the X click to GTWgui
    X click means that a user can click the X key to toggle the cursor, this
    is now located in GTWgui where it belongs. All GUI windows will now have
    a X button by default to cloose them.

--- [resources]/GTWgui/c_window.lua
+++ [resources]/GTWgui/c_window.lua
@@ -15,7 +15,7 @@
 ]]--
 
 --[[ Make a custom GTW window ]]--
-function createWindow(x, y, width, height, text, relative, source_resource )
+function createWindow(x, y, width, height, text, relative, source_resource)
 	local window = guiCreateStaticImage(x, y, width, height, "img/back.png", relative )
 	local winTitle = guiCreateLabel((width/2)-((string.len(text)*7)/2), 3, (string.len(text)*10), 20, text, relative, window)
 	local winTitleFont = guiCreateFont( "fonts/RobotoSlab-Bold.ttf", 11 )
@@ -26,13 +26,29 @@ function createWindow(x, y, width, height, text, relative, source_resource )
 		triggerServerEvent("GTWgui.addToRefreshList", resourceRoot, source_resource)
 	end
 
+	-- Add a cloose button
+	local close_button = guiCreateButton(width-30, 0, 30, 28, "X", false, window)
+	addEventHandler("onClientGUIClick", close_button, function()
+		guiSetVisible(window, false)
+	end)
+
 	-- Apply the new font style of the GUI
-	guiSetFont( winTitle, winTitleFont )
+	guiSetFont(winTitle, winTitleFont)
 	return window
 end
 
 --[[ Walrus default font ]]--
 function setDefaultFont(inputElement, size)
 	local font = guiCreateFont("fonts/RobotoSlab-Bold.ttf", size)
-	guiSetFont( inputElement, font )
+	guiSetFont(inputElement, font)
 end
+
+--[[ Toggle the cursor (globally) ]]--
+function toggleCursor()
+	if isCursorShowing(localPlayer) then
+    		showCursor(false, false)
+    	else
+    		showCursor(true, true)
+    	end
+end
+bindKey("x", "down", toggleCursor, "Toggle cursor")
