bbe580508b651f8c59bc42de48cf6716e38f4969
Mr_Moose <themoose@404rq.com>
2015-09-07 15:55:24 +0200

    [Update/New] Added ability to sell from fast food trucks
    Players can now sell ice cream and hotdogs from fast food trucks using
    the command /sell

--- [resources]/GTWfastfood/food_c.lua
+++ [resources]/GTWfastfood/food_c.lua
@@ -14,110 +14,103 @@
 ********************************************************************************
 ]]--
 
-GUIEditor_Window = {}
-GUIEditor_Button = {}
+-- Define the menu GUI window
+local sx,sy = guiGetScreenSize()
+local win_menu = exports.GTWgui:createWindow((sx-320)/2, (sy-297)/2, 320, 297, "Well Stacked Pizza Co. Menu", false)
+guiSetVisible(win_menu, false)
 
-x,y = guiGetScreenSize()
-GUIEditor_Window[1] = exports.GTWgui:createWindow((x-320)/2,(y-297)/2, 320, 297, "Well Stacked Pizza Co. Menu", false)
-guiSetVisible(GUIEditor_Window[1],false)
-GUIEditor_Button[1] = guiCreateButton(210,257,100,36,"Close",false,GUIEditor_Window[1])
-GUIEditor_Button[2] = guiCreateButton(10,100,160,36, "Buster("..prices[1].."$)",false,GUIEditor_Window[1])
-GUIEditor_Button[3] = guiCreateButton(10,217,160,36, "Double D-Luxe("..prices[2].."$)",false,GUIEditor_Window[1])
-GUIEditor_Button[4] = guiCreateButton(170,100,160,36, "Full rack("..prices[3].."$)",false,GUIEditor_Window[1])
-GUIEditor_Button[5] = guiCreateButton(170,217,160,36, "Sallad meal("..prices[4].."$)",false,GUIEditor_Window[1])
+-- Define the menu buttons
+btn_close = guiCreateButton(210,257,100,36,"Close",false,win_menu)
+btn_choice_1 = guiCreateButton(10,100,160,36, "Buster("..prices[1].."$)",false,win_menu)
+btn_choice_2 = guiCreateButton(10,217,160,36, "Double D-Luxe("..prices[2].."$)",false,win_menu)
+btn_choice_3 = guiCreateButton(170,100,160,36, "Full rack("..prices[3].."$)",false,win_menu)
+btn_choice_4 = guiCreateButton(170,217,160,36, "Sallad meal("..prices[4].."$)",false,win_menu)
 
-function showGUI(hitPlayer, type)
-	if type == "cluckin_bell" then
-		guiSetText(GUIEditor_Window[1], "Cluckin' Bell Menu")
-		guiSetText(GUIEditor_Button[2], "Cluckin' Little Meal("..prices[1].."$)")
-		guiSetText(GUIEditor_Button[3], "Cluckin' Big Meal("..prices[2].."$)")
-		guiSetText(GUIEditor_Button[4], "Cluckin' Huge Meal("..prices[3].."$)")
-		guiSetText(GUIEditor_Button[5], "Salad Meal("..prices[4].."$)")
-		-- menu images
+-- Apply GTWgui styles
+exports.GTWgui:setDefaultFont(btn_close, 10)
+exports.GTWgui:setDefaultFont(btn_choice_1, 10)
+exports.GTWgui:setDefaultFont(btn_choice_2, 10)
+exports.GTWgui:setDefaultFont(btn_choice_3, 10)
+exports.GTWgui:setDefaultFont(btn_choice_4, 10)
+
+--[[ Update the GUI depending on restaurant type ]]--
+function initialize_menu_choices(r_type)
+	-- Set the text and prices on buttons and window title
+	guiSetText(win_menu, menu_choices[r_type]["title"])
+	guiSetText(btn_choice_1, menu_choices[r_type]["choice_1"][1]..", "..prices[1].."$")
+	guiSetText(btn_choice_2, menu_choices[r_type]["choice_2"][1]..", "..prices[2].."$)")
+	guiSetText(btn_choice_3, menu_choices[r_type]["choice_3"][1]..", "..prices[3].."$)")
+	guiSetText(btn_choice_4, menu_choices[r_type]["choice_4"][1]..", "..prices[4].."$)")
+
+	-- Destroy current images if any
+	if img1 and isElement(img1) then destroyElement(img1) end
+	if img2 and isElement(img2) then destroyElement(img2) end
+	if img3 and isElement(img3) then destroyElement(img3) end
+	if img4 and isElement(img4) then destroyElement(img4) end
+
+	-- Make new images, (load from files when needed)
+	img1 = guiCreateStaticImage(10, 23, 160, 77, menu_choices[r_type]["choice_1"][2], false, win_menu)
+	img2 = guiCreateStaticImage(10,140, 160, 77, menu_choices[r_type]["choice_2"][2], false, win_menu)
+	img3 = guiCreateStaticImage(170, 23, 160, 77, menu_choices[r_type]["choice_3"][2], false, win_menu)
+	img4 = guiCreateStaticImage(170, 140, 160, 77, menu_choices[r_type]["choice_4"][2], false, win_menu)
+end
+
+--[[ Open the menu GUI to choose food ]]--
+function open_menu(plr, r_type)
+	initialize_menu_choices(r_type)
+	--[[else
 		if img1 and isElement(img1) then destroyElement(img1) end
 		if img2 and isElement(img2) then destroyElement(img2) end
 		if img3 and isElement(img3) then destroyElement(img3) end
 		if img4 and isElement(img4) then destroyElement(img4) end
-		img1 = guiCreateStaticImage(10, 23, 160, 77, "img/CLUHEAL.png", false, GUIEditor_Window[1])
-		img2 = guiCreateStaticImage(10,140, 160, 77, "img/CLUHIG.png", false, GUIEditor_Window[1])
-		img3 = guiCreateStaticImage(170, 23, 160, 77, "img/CLULOW.png", false, GUIEditor_Window[1])
-		img4 = guiCreateStaticImage(170, 140, 160, 77, "img/CLUMED.png", false, GUIEditor_Window[1])
-	elseif type == "burger" then
-		guiSetText(GUIEditor_Window[1], "Burger Shot Menu")
-		guiSetText(GUIEditor_Button[2], "Moo kids Meal("..prices[1].."$)")
-		guiSetText(GUIEditor_Button[3], "Beef Tower Meal("..prices[2].."$)")
-		guiSetText(GUIEditor_Button[4], "Meat Stack Meal("..prices[3].."$)")
-		guiSetText(GUIEditor_Button[5], "Salad Meal("..prices[4].."$)")
-		-- menu images
-		if img1 and isElement(img1) then destroyElement(img1) end
-		if img2 and isElement(img2) then destroyElement(img2) end
-		if img3 and isElement(img3) then destroyElement(img3) end
-		if img4 and isElement(img4) then destroyElement(img4) end
-		img1 = guiCreateStaticImage(10, 23, 160, 77, "img/BURHEAL.png", false, GUIEditor_Window[1])
-		img2 = guiCreateStaticImage(10, 140, 160, 77, "img/BURHIG.png", false, GUIEditor_Window[1])
-		img3 = guiCreateStaticImage(170, 23, 160, 77, "img/BURLOW.png", false, GUIEditor_Window[1])
-		img4 = guiCreateStaticImage(170, 140, 160, 77, "img/BURMED.png", false, GUIEditor_Window[1])
-	else
-		if img1 and isElement(img1) then destroyElement(img1) end
-		if img2 and isElement(img2) then destroyElement(img2) end
-		if img3 and isElement(img3) then destroyElement(img3) end
-		if img4 and isElement(img4) then destroyElement(img4) end
-		img1 = guiCreateStaticImage(10, 23, 160, 77, "img/PIZHEAL.png", false, GUIEditor_Window[1])
-		img2 = guiCreateStaticImage(10, 140, 160, 77, "img/PIZHIG.png", false, GUIEditor_Window[1])
-		img3 = guiCreateStaticImage(170, 23, 160, 77, "img/PIZLOW.png", false, GUIEditor_Window[1])
-		img4 = guiCreateStaticImage(170, 140, 160, 77, "img/PIZMED.png", false, GUIEditor_Window[1])
-	end
-	guiSetVisible(GUIEditor_Window[1], true)
+		img1 = guiCreateStaticImage(10, 23, 160, 77, "img/PIZHEAL.png", false, win_menu)
+		img2 = guiCreateStaticImage(10, 140, 160, 77, "img/PIZHIG.png", false, win_menu)
+		img3 = guiCreateStaticImage(170, 23, 160, 77, "img/PIZLOW.png", false, win_menu)
+		img4 = guiCreateStaticImage(170, 140, 160, 77, "img/PIZMED.png", false, win_menu)
+	end]]
+	guiSetVisible(win_menu, true)
 	showCursor(true)
 end
 addEvent("GTWfastfood.gui.show",true)
-addEventHandler("GTWfastfood.gui.show",getRootElement(),showGUI)
+addEventHandler("GTWfastfood.gui.show", root, open_menu)
 
-exports.GTWgui:setDefaultFont(GUIEditor_Button[1], 10)
-exports.GTWgui:setDefaultFont(GUIEditor_Button[2], 10)
-exports.GTWgui:setDefaultFont(GUIEditor_Button[3], 10)
-exports.GTWgui:setDefaultFont(GUIEditor_Button[4], 10)
-exports.GTWgui:setDefaultFont(GUIEditor_Button[5], 10)
-
-function hideGUI()
-	guiSetVisible(GUIEditor_Window[1], false)
+function close_menu()
+	guiSetVisible(win_menu, false)
 	showCursor(false)
 end
-addEvent("GTWfastfood.gui.hide",true)
-addEventHandler("GTWfastfood.gui.hide",getRootElement(),hideGUI)
+addEvent("GTWfastfood.gui.hide", true)
+addEventHandler("GTWfastfood.gui.hide", root, close_menu)
 
-addEventHandler("onClientGUIClick",getRootElement(),
-function(player)
-	local player2 = getLocalPlayer()
-	local money = getPlayerMoney(player2)
-   	if(source == GUIEditor_Button[1]) then
-   		guiSetVisible(GUIEditor_Window[1],false)
+--[[ Handle menu click ]]--
+function menu_click()
+	-- Make sure that the player can afford his food
+	local money = getPlayerMoney(localPlayer)
+
+	-- Shall we close this menu?
+   	if source == btn_close then
+   		guiSetVisible(win_menu, false)
    		showCursor(false)
-   	elseif(source == GUIEditor_Button[2]) then
-   		if(money > prices[1]) and not isTimer(cooldown) then
-   			triggerServerEvent("GTWfastfood.buy", getLocalPlayer(), prices[1], health[1])
-   			cooldown = setTimer(function() end, 500, 1)
-  		end
-  	elseif(source == GUIEditor_Button[3]) then
-   		if(money > prices[2]) and not isTimer(cooldown) then
-   			triggerServerEvent("GTWfastfood.buy", getLocalPlayer(), prices[2], health[2])
-   			cooldown = setTimer(function() end, 500, 1)
-  		end
-  	elseif(source == GUIEditor_Button[4]) then
-   		if(money > prices[3]) and not isTimer(cooldown) then
-   			triggerServerEvent("GTWfastfood.buy", getLocalPlayer(), prices[3], health[3])
-   			cooldown = setTimer(function() end, 500, 1)
-  		end
-  	elseif(source == GUIEditor_Button[5]) then
-   		if(money > prices[4]) and not isTimer(cooldown) then
-   			triggerServerEvent("GTWfastfood.buy", getLocalPlayer(), prices[4], health[4])
-   			cooldown = setTimer(function() end, 500, 1)
-  		end
-  	end
-end)
+		return
+	end
 
-function cancelPedDamage(attacker)
-	setTimer(setElementHealth, 5000, 1, source, 100)
+	-- Alright, let's continue shopping, can I take your order?
+	local ID_choice = 1
+   	if source == btn_choice_1 then ID_choice = 1 end
+	if source == btn_choice_2 then ID_choice = 2 end
+	if source == btn_choice_3 then ID_choice = 3 end
+	if source == btn_choice_4 then ID_choice = 4 end
+
+	-- Take care of the order server side
+   	if money > prices[ID_choice] and not isTimer(cooldown) then
+   		triggerServerEvent("GTWfastfood.buy", localPlayer, prices[ID_choice], health[ID_choice])
+   		cooldown = setTimer(function() end, 200, 1)
+  	end
+end
+addEventHandler("onClientGUIClick", root, menu_click)
+
+--[[ Protect fastfood workers from being killed ]]--
+function protect_worker(attacker)
+	if not getElementData(source, "GTWfastfood.isWorker") then return end
 	cancelEvent() -- cancel any damage done to peds
 end
-addEventHandler("onClientPedWasted", resourceRoot, cancelPedDamage)
+addEventHandler("onClientPedWasted", root, protect_worker)
