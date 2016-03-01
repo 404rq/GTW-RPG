05981daaee173631504f0190df3dc3d73b05d886
Mr_Moose <themoose@404rq.com>
2016-03-01 16:44:37 +0100

    misc: error message in case no time was specified or 0

--- [resources]/GTWjail/jail_s.lua
+++ [resources]/GTWjail/jail_s.lua
@@ -38,7 +38,10 @@ ammo 		= { }
 function Jail(crim, time, police_dept, reason, admin)
 	if not crim or not isElement(crim) or getElementType(crim) ~= "player" then return end
 	if not police_dept then outputServerLog("GTW-RPG: (Wanted) Please specify a police department") end
-	if tonumber(time) and math.floor(tonumber(time)) > 0 then time = tonumber(time) else return end
+	if tonumber(time) and math.floor(tonumber(time)) > 0 then time = tonumber(time) else
+		outputServerLog("GTW-RPG: (Wanted) no time specified")
+		time = 60
+	end
 	if jail_data.is_jailed[crim] then
 		exports.GTWtopbar:dm(getPlayerName(crim).." is already in jail!", crim, 255, 150, 0)
 		return
