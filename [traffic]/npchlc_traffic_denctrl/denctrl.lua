671cf46eac489ad1308e242aa0ae7d43e344e6f6
Mr_Moose <admin@99stack.com>
2016-04-07 18:51:53 +0200

    update: trafic density depends on time of day

--- [traffic]/npchlc_traffic_denctrl/denctrl.lua
+++ [traffic]/npchlc_traffic_denctrl/denctrl.lua
@@ -24,3 +24,30 @@ function setTDensity()
 	exports.npc_traffic:setTrafficDensity("planes", 0.04)
 end
 setTimer(setTDensity, 1000, 30)
+
+--[[ Keep density updated based on time per day ]]--
+function updateDensity()
+	local multiplier = 1
+	local h,m = getTime()
+	if h < 4 then multiplier = 0.2
+	elseif h < 5 then multiplier = 0.6
+	elseif h < 6 then multiplier = 1
+	elseif h < 7 then multiplier = 3
+	elseif h < 9 then multiplier = 5
+	elseif h < 10 then multiplier = 3
+	elseif h < 11 then multiplier = 4
+	elseif h < 12 then multiplier = 3
+	elseif h < 13 then multiplier = 6
+	elseif h < 15 then multiplier = 4
+	elseif h < 18 then multiplier = 5
+	elseif h < 19 then multiplier = 4
+	elseif h < 20 then multiplier = 2.5
+	elseif h < 21 then multiplier = 1
+	elseif h < 22 then multiplier = 0.8 end
+	
+	exports.npc_traffic:setTrafficDensity(0.02*multiplier)
+	exports.npc_traffic:setTrafficDensity("peds", 0.06*multiplier)
+	exports.npc_traffic:setTrafficDensity("boats", 0.04*multiplier)
+	exports.npc_traffic:setTrafficDensity("planes", 0.04*multiplier)
+end
+setTimer(updateDensity, 5000, 0)
