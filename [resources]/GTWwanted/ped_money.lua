25a9a678865dc1145bd28b7e74ba58efcff874ca
Mr_Moose <admin@99stack.com>
2016-05-15 20:59:50 +0200

    update: increased profit for killing violent bots

--- [resources]/GTWwanted/ped_money.lua
+++ [resources]/GTWwanted/ped_money.lua
@@ -20,13 +20,22 @@ profit = {}
 
 --[[ Triggers whenever a ped is killed ]]--
 function killedPed(totalAmmo, killer, killerWeapon, bodypart, stealth)
+	-- Do not drop money for law units
+	if killer and getElementType(killer) == "player" and getPlayerTeam(killer) and getPlayerTeam(killer) == getTeamFromName("Government") then return end
+
+	-- Create a money pickup at the position of the dead bot
 	local x,y,z = getElementPosition(source)
-	profit[killer] = math.random(1,200)
-    local pickup = createPickup(x, y, z, 3, 1212, 120000, profit[killer])
-    if pickup then
-    	setTimer(destroyMinutePickup, 45000, 1, pickup)
-    	addEventHandler("onPickupHit", pickup, givePickupMoney)
-    end
+	profit[killer] = math.random(10,500)
+	
+	-- Increase payment if the dead bot was an outlaw
+	if getElementData(source, "GTWoutlaws.vBot") then profit[killer] = math.random(500,5000) end
+	
+	-- Make the pickup and make sure it's removed after 2 minutes if not picked up
+	local pickup = createPickup(x, y, z, 3, 1212, 120000, profit[killer])
+	if pickup then
+		setTimer(destroyMinutePickup, 120*1000, 1, pickup)
+		addEventHandler("onPickupHit", pickup, givePickupMoney)
+	end
 end
 addEventHandler("onPedWasted", root, killedPed)
 
@@ -40,9 +49,9 @@ end
 
 --[[ Make the robber wanted on money pickup ]]--
 function givePickupMoney(plr)
-    if not profit[plr] then profit[plr] = math.random(1,50) end
-    removeEventHandler("onPickupHit", source, givePickupMoney)
-    destroyElement(source)
+	if not profit[plr] then profit[plr] = math.random(1,50) end
+	removeEventHandler("onPickupHit", source, givePickupMoney)
+	destroyElement(source)
    	givePlayerMoney(plr, profit[plr])
 
    	-- Get wanted for stealing money
